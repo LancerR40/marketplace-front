c793611172985152f0c31c5e21771bd662545b7e
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-09-24 02:37:24 -0400

    Added vendor login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import VendorSignup from './pages/vendor/Signup'
+import VendorLogin  from './pages/vendor/Login'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/vendor/signup' element={<VendorSignup />} />
+        <Route path='/vendor/login'  element={<VendorLogin  />} />
       </Routes>
     </Router>
   );
--- src/pages/vendor/Login.js
+++ src/pages/vendor/Login.js
@@ -1,11 +1,11 @@
 import { H1, FormGroup, Label, Input, Button } from '../../components/ui'
 import { Link } from 'react-router-dom'
 
-const Signup = () => {
+const Login = () => {
   return (
     <div className="flex justify-center items-center overflow-auto h-screen bg-gray-100" style={{ minHeight: 640 }}>
       <form className="py-8 px-4 sm:px-8 w-full max-w-md rounded shadow-sm bg-white">
-        <H1 className="text-center uppercase text-gray-700">Crear una cuenta</H1>
+        <H1 className="text-center uppercase text-gray-700">Iniciar sesión</H1>
 
         <div className='mt-8'>
           <FormGroup>
@@ -19,20 +19,15 @@ const Signup = () => {
             <Input type='password' name='password' placeholder='********' />
           </FormGroup>
 
-          <FormGroup>
-            <Label>Confirmar contraseña:</Label>
-            <Input type='password' name='passwordConfirm' placeholder='********' />
-          </FormGroup>
-
           <div className='text-center'>
-            <Button className='w-fit px-8 py-3 uppercase'>Registrarse</Button>
+            <Button className='w-fit px-8 py-3 uppercase'>Continuar</Button>
           </div>
         </div>
 
-        <Link className='block underline mt-4 text-center text-lg text-blue-500' to='/vendor/login'>Iniciar sesión</Link>
+        <Link className='block underline mt-4 text-center text-lg text-blue-500' to='/vendor/signup'>Registarse</Link>
       </form>
     </div>
   )
 }
 
-export default Signup
+export default Login
