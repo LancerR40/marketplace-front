153425778e7d519d0b096da707df86329374f706
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-09-25 21:27:48 -0400

    Hot fix

--- src/components/product/Card.js
+++ src/components/product/Card.js
@@ -3,7 +3,7 @@ import image from '../../assets/image-1.jpg'
 
 const Card = ({ productName, sku, price, vendorName }) => {
   return (
-    <div className="border border-gray-200 rounded bg-gray-50 transition-all hover:border-2 hover:border-blue-500 hover:-translate-y-1">
+    <div className="border border-gray-200 rounded bg-gray-50 transition-all hover:border-2 hover:border-blue-500 hover:-translate-y-1" style={{ maxWidth: 250 }}>
       <div>
         <img src={image} className='object-fit' style={{ width: 250, height: 150 }} />
       </div>
